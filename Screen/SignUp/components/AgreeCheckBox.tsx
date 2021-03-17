f8d87bbe8c19ee07277aae812c14e7dc4a379357
purplecode2020 <purplecode2020@gmail.com>
2021-03-17 20:16:02 +0900

    [FIX] checkbox

--- Screen/SignUp/components/AgreeCheckBox.tsx
+++ Screen/SignUp/components/AgreeCheckBox.tsx
@@ -3,10 +3,22 @@ import styled from 'styled-components/native';
 import CheckBox from 'react-native-check-box';
 
 const AgreeCheckBox = () => {
-  const [all, setAll] = useState(false);
-  const [service, setService] = useState(false);
-  const [info, setInfo] = useState(false);
-  const [event, setEvent] = useState(false);
+  const [checked, setChecked] = useState<{ [key: string]: boolean }>({
+    service: false,
+    info: false,
+    event: false,
+  });
+
+  const allCheck = checked.service && checked.info && checked.event;
+
+  const setAll = () => {
+    const changeValue = !allCheck;
+    setChecked({ service: changeValue, info: changeValue, event: changeValue });
+  };
+
+  const itemCheck = (key: string) => {
+    setChecked((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
 
   return (
     <Container>
@@ -14,37 +26,35 @@ const AgreeCheckBox = () => {
         rightText="아래 약관에 모두 동의합니다"
         rightTextStyle={{ fontSize: 12 }}
         checkBoxColor="#E3E3E3"
-        isChecked={all}
-        onClick={() => {
-          setAll(!all);
-        }}
+        isChecked={allCheck}
+        onClick={setAll}
       />
       <Line />
       <CheckBox
         rightText="서비스 이용약관 (필수)"
         rightTextStyle={{ fontSize: 12, textDecorationLine: 'underline' }}
         checkBoxColor="#E3E3E3"
-        isChecked={service}
+        isChecked={checked.service}
         onClick={() => {
-          setService(!service);
+          itemCheck('service');
         }}
       />
       <CheckBox
         rightText="개인정보 처리 방침 (필수)"
         rightTextStyle={{ fontSize: 12, textDecorationLine: 'underline' }}
         checkBoxColor="#E3E3E3"
-        isChecked={info}
+        isChecked={checked.info}
         onClick={() => {
-          setInfo(!info);
+          itemCheck('info');
         }}
       />
       <CheckBox
         rightText="이벤트 등 프로모션 알림 메일 수신 (선택)"
         rightTextStyle={{ fontSize: 12 }}
         checkBoxColor="#E3E3E3"
-        isChecked={event}
+        isChecked={checked.event}
         onClick={() => {
-          setEvent(!event);
+          itemCheck('event');
         }}
       />
     </Container>
