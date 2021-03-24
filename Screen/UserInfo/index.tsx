f3eaa7a07ade4b17aac37766c1c3015481a256ee
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-03-24 13:38:01 +0900

    [ADD] 회원정보 text

--- Screen/UserInfo/index.tsx
+++ Screen/UserInfo/index.tsx
@@ -5,11 +5,17 @@ import UserName from '../../Component/UserName';
 const UserInfo = () => {
   return (
     <UserInfoContainer headerTitle="회원정보">
+      <SmallLine />
       <UserImageContainer>
         <UserImage source={require('./image/profile.png')} />
         <UserName title="사용자" />
       </UserImageContainer>
       <Line />
+      <Text>회원정보 수정</Text>
+      <SmallLine />
+      <Text>로그아웃</Text>
+      <SmallLine />
+      <LinkText>회원탈퇴</LinkText>
     </UserInfoContainer>
   );
 };
@@ -31,4 +37,25 @@ const Line = styled.Text`
   background-color: #e3e3e3;
 `;
 
+const SmallLine = styled.Text`
+  width: 100%;
+  height: 1px;
+  background-color: #e3e3e3;
+`;
+
+const Text = styled.Text`
+  font-size: 12px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  padding-left: 24px;
+`;
+
+const LinkText = styled.Text`
+  color: #b4b4b4;
+  font-size: 10px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  padding-left: 24px;
+  text-decoration: #b4b4b4 underline;
+`;
 export default UserInfo;
