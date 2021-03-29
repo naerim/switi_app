04e3ef4a07aaba710372029cced967dfc5bd1b71
naerim <naerim1119@gmail.com>
2021-03-30 02:02:19 +0900

    [ADD] study 데미데이터 생성

--- Screen/Home/index.tsx
+++ Screen/Home/index.tsx
@@ -4,8 +4,11 @@ import styled from 'styled-components/native';
 const Home = () => {
   return (
     <Container>
+      <CategoryArea>
+        <CategoryText>카테고리영역</CategoryText>
+      </CategoryArea>
       <Content>
-        <TestText>test</TestText>
+        <CategoryText>flatList 영역</CategoryText>
       </Content>
     </Container>
   );
@@ -19,8 +22,15 @@ const Container = styled.SafeAreaView`
 const Content = styled.View`
   margin: 0 24px;
 `;
-const TestText = styled.Text`
-  font-size: 30px;
+
+const CategoryArea = styled.View`
+  margin-left: 24px;
+  background-color: beige;
+  padding: 24px 0;
+`;
+
+const CategoryText = styled.Text`
+  font-size: 12px;
 `;
 
 export default Home;
