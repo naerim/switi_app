60b676c3dbb78db7e9186651c447493bc85192f2
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-04-05 14:36:18 +0900

    [ADD] Alarm Button

--- Screen/Alarm/index.tsx
+++ Screen/Alarm/index.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import styled from 'styled-components/native';
 import { Switch, View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import BasicContainer from '../../Component/BasicContainer';
 import { useGoMyPage } from '../../util/navigationHooks';
+import ColorButton from '../../Component/ColorButton';
+
 const Alarm = () => {
   const [switchValue, setSwitchValue] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const onPress = () => console.log('설정으로 이동');
   const toggleSwitch = (value: any) => {
     setSwitchValue(value);
   };
@@ -13,7 +17,18 @@ const Alarm = () => {
 
   return (
     <BasicContainer onPress={goMyPage} display headerTitle="알림설정">
-      <SafeAreaView style={{ flex: 1 }}>
+      <TopTextContainer>
+        <BasicText>이런, 기기 알림이 꺼져있네요!</BasicText>
+      </TopTextContainer>
+      <ButtonContainer>
+        <ColorButton
+          text="기기 알림 설정하기"
+          onPress={onPress}
+          loading={isLoading}
+          color="#86E3C3"
+        />
+      </ButtonContainer>
+      <SwitchContainer>
         <View style={styles.container}>
           <Text>{switchValue ? 'Switch is ON' : 'Switch is OFF'}</Text>
           <Switch
@@ -22,7 +37,7 @@ const Alarm = () => {
             value={switchValue}
           />
         </View>
-      </SafeAreaView>
+      </SwitchContainer>
     </BasicContainer>
   );
 };
@@ -34,5 +49,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
+const TopTextContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const SwitchContainer = styled.View`
+  background-color: skyblue;
+  flex: 10;
+`;
+
+const BasicText = styled.Text`
+  font-size: 12px;
+`;
 
 export default Alarm;
