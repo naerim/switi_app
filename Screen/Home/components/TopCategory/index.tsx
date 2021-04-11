913bd846454fb8975eb8bca9094cc5b05b2eb2b1
naerim <naerim1119@gmail.com>
2021-04-11 21:46:50 +0900

    [FIX] 홈페이지 카테고리 data 설정

--- Screen/Home/components/TopCategory/index.tsx
+++ Screen/Home/components/TopCategory/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { Area, InterestList, TargetList } from '../../../../Data';
+import SelectFlatList from '../../../../Component/SelectFlatList';
+import BasicModal from '../../../../Component/BasicModal';
 
 const TopCategory = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const onPress = (data: { key: number; name: string }[]) => {
+    setModalVisible(true);
+    setSelectData(data);
+    //addTagList();
+  };
+  const closeModal = () => setModalVisible(false);
+
+  // const [interest, setInterest] = useState<number[]>([]);
+  // const [area, setArea] = useState<number[]>([]);
+  // const [target, setTarget] = useState<number[]>([]);
+  const [select, setSelect] = useState<number[]>([]);
+  const [selectData, setSelectData] = useState<{ key: number; name: string }[]>(
+    []
+  );
+  // const [tag, setTag] = useState<string[]>([]);
+
+  // const addTagList = () => {
+  //   const nameList = () => interest.map((i) => InterestList[i].name);
+  //   const nameList1 = () => area.map((i) => Area[i].name);
+  //   const array = nameList().concat(nameList1());
+  //   setTag(array);
+  // };
+
   const categoryData = [
     {
       title: '카테고리',
@@ -20,18 +46,31 @@ const TopCategory = () => {
 
   return (
     <Container>
-      {categoryData.map(({ title }) => (
-        <Content key={title}>
-          <Title>{title}</Title>
-        </Content>
-      ))}
+      <SelectContainer>
+        {categoryData.map(({ title, data }) => (
+          <Content key={title} onPress={() => onPress(data)}>
+            <Title>{title}</Title>
+            <BasicModal modalVisible={modalVisible}>
+              <SelectFlatList
+                data={selectData}
+                select={select}
+                setSelect={setSelect}
+                closeModal={closeModal}
+              />
+            </BasicModal>
+          </Content>
+        ))}
+      </SelectContainer>
     </Container>
   );
 };
 
 const Container = styled.View`
-  flex-direction: row;
   margin: 24px;
+`;
+
+const SelectContainer = styled.View`
+  flex-direction: row;
 `;
 
 const Content = styled.TouchableOpacity`
@@ -44,6 +83,14 @@ const Content = styled.TouchableOpacity`
 const Title = styled.Text`
   color: #2b2b2b;
   font-size: 12px;
+`;
+
+const Div = styled.View`
+  flex-direction: column;
+`;
+
+const Desc = styled.Text`
+  color: red;
 `;
 
 export default TopCategory;
