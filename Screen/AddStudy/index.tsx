8f6532abb3d56f34c2468a610118ab6d894ca82f
naerim <naerim1119@gmail.com>
2021-04-13 16:27:16 +0900

    [ADD] 모집글 작성 - 헤더 추가

--- Screen/AddStudy/index.tsx
+++ Screen/AddStudy/index.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { useGoHome } from '../../util/navigationHooks';
+import Header from './components/Header';
+import { Platform } from 'react-native';
 
 const AddStudy = () => {
   const goHome = useGoHome();
   return (
-    <Container>
+    <Container style={{ paddingTop: Platform.OS === 'ios' ? 0 : 20 }}>
+      <Header onPress={goHome} />
       <Title>스터디 추가 페이지</Title>
-      <PrevButton title="x" onPress={goHome} />
     </Container>
   );
 };
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Container = styled.SafeAreaView`
+  background-color: white;
 `;
 
 const Title = styled.Text`
   font-size: 14px;
 `;
-
-const PrevButton = styled.Button``;
 
 export default AddStudy;
