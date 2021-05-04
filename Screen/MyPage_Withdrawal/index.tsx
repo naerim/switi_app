2d1d3aaf76020f10d6bb35ef97b7fc83fe1aac0c
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-05-05 01:11:15 +0900

    [Fix] 회원탈퇴 screen
    ios Android 모두 적용 가능한 스크린으로 수정

--- Screen/MyPage_Withdrawal/index.tsx
+++ Screen/MyPage_Withdrawal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import BasicContainer from '../../Component/BasicContainer';
+import BasicContainer from '../../Component/MypageContainer';
 import { useGoMyPageUserInfo } from '../../util/navigationHooks';
 import TwoButton from './twoButton';
 import RadioButtonContainer from './radioButtonContainer';
@@ -22,44 +22,52 @@ const MyPage_Withdrawal = () => {
 
   return (
     <BasicContainer headerTitle="회원탈퇴" display onPress={goMyPage}>
-      <GuideContainer>
-        <Question>정말 이대로 그만두시겠습니까? :(</Question>
-        <Answer>
-          회원탈퇴 시 내 모집글, 스크랩, 당도 등 모든 관련 정보가 모두
-          삭제됩니다.
-        </Answer>
-      </GuideContainer>
-      <ReasonContainer>
-        <Question>탈퇴하려는 이유가 무엇인가요?</Question>
-        <RadioButtonContainer input={{ reason, setReason }} />
-        <ReasonText input={reasonInput} />
-      </ReasonContainer>
-      <ButtonContainer>
-        <TwoButton
-          text="나중에 할께요"
-          onPress={goMyPage}
-          loading={isLoading}
-        />
-        <TwoButton
-          text="네, 탈퇴할래요"
-          onPress={onPressWithdrawal}
-          loading={isLoading}
-          color="#86E3C3"
-          textColor="white"
-        />
-      </ButtonContainer>
-      <BasicModal modalVisible={modalVisible} closeModal={closeModal}>
-        <ModalTextContainer>
-          <ModalBigText>회원탈퇴가 완료되었습니다</ModalBigText>
-          <ModalSmallText>계정이 삭제되었습니다.</ModalSmallText>
-          <ModalSmallText>
-            다음에 다시 볼 수 있었으면 좋겠네요 :)
-          </ModalSmallText>
-        </ModalTextContainer>
-      </BasicModal>
+      <MarginContainer>
+        <GuideContainer>
+          <Question>정말 이대로 그만두시겠습니까? :(</Question>
+          <Answer>
+            회원탈퇴 시 내 모집글, 스크랩, 당도 등 모든 관련 정보가 모두
+            삭제됩니다.
+          </Answer>
+        </GuideContainer>
+        <ReasonContainer>
+          <Question>탈퇴하려는 이유가 무엇인가요?</Question>
+          <RadioButtonContainer input={{ reason, setReason }} />
+          <ReasonText input={reasonInput} />
+        </ReasonContainer>
+        <ButtonContainer>
+          <TwoButton
+            text="나중에 할께요"
+            onPress={goMyPage}
+            loading={isLoading}
+          />
+          <TwoButton
+            text="네, 탈퇴할래요"
+            onPress={onPressWithdrawal}
+            loading={isLoading}
+            color="#86E3C3"
+            textColor="white"
+          />
+        </ButtonContainer>
+        <BasicModal modalVisible={modalVisible} closeModal={closeModal}>
+          <ModalTextContainer>
+            <ModalBigText>회원탈퇴가 완료되었습니다</ModalBigText>
+            <ModalSmallText>계정이 삭제되었습니다.</ModalSmallText>
+            <ModalSmallText>
+              다음에 다시 볼 수 있었으면 좋겠네요 :)
+            </ModalSmallText>
+          </ModalTextContainer>
+        </BasicModal>
+      </MarginContainer>
     </BasicContainer>
   );
 };
+
+const MarginContainer = styled.View`
+  flex: 1;
+  margin-right: 24px;
+  margin-left: 24px;
+`;
 
 const Question = styled.Text`
   font-size: 16px;
