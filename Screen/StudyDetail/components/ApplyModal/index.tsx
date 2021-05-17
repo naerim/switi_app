be76500821d24e701aa892e54c4cc499fd07ed7c
naerim <naerim1119@gmail.com>
2021-05-17 11:53:27 +0900

    [FIX] 스터디 모집신청 Input, Button

--- Screen/StudyDetail/components/ApplyModal/index.tsx
+++ Screen/StudyDetail/components/ApplyModal/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import BasicModal from '../../../../Component/BasicModal';
+import Input from '../../../AddStudy/components/Input';
+import useInput from '../../../../util/useInput';
+import BasicButton from '../../../../Component/BasicButton';
 
 interface Props {
   modalVisible: boolean;
@@ -8,17 +11,31 @@ interface Props {
 }
 
 const ApplyModal: React.FC<Props> = ({ modalVisible, closeModal }) => {
+  const Contact = useInput('');
+  const Reason = useInput('');
   return (
     <BasicModal modalVisible={modalVisible} closeModal={closeModal}>
       <Container>
-        <Title>연락처(카카오톡 아이디 or 전화번호)</Title>
+        <Input
+          title="연락처(카카오톡 아이디 or 전화번호)"
+          input={Contact}
+          placeholder="카카오톡 아이디 혹은 전화번호를 입력해주세요"
+        />
+        <Input
+          title="신청사유"
+          input={Reason}
+          placeholder="카카오톡 아이디 혹은 전화번호를 입력해주세요"
+        />
+        <Title>신청 후 연락처와 신청사유는 수정이 어려워요.</Title>
       </Container>
+      <BasicButton text="신청하기" onPress={closeModal} />
     </BasicModal>
   );
 };
 
 const Container = styled.View`
-  padding: 24px 24px;
+  padding: 24px 0;
+  margin-bottom: 60px;
 `;
 
 const Title = styled.Text`
