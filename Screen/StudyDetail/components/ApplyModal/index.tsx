1b02324c1e07243368428875ec5c8b79c64846d0
naerim <naerim1119@gmail.com>
2021-05-19 15:08:05 +0900

    [ADD] 모집 신청완료 모달창

--- Screen/StudyDetail/components/ApplyModal/index.tsx
+++ Screen/StudyDetail/components/ApplyModal/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import BasicModal from '../../../../Component/BasicModal';
 import Input from '../../../AddStudy/components/Input';
 import useInput from '../../../../util/useInput';
 import BasicButton from '../../../../Component/BasicButton';
 import ReasonInput from './ReasonInput';
+import ApplyDoneModal from './ApplyDoneModal';
 
 interface Props {
   modalVisible: boolean;
@@ -14,23 +15,42 @@ interface Props {
 const ApplyModal: React.FC<Props> = ({ modalVisible, closeModal }) => {
   const Contact = useInput('');
   const Reason = useInput('');
+  const [DoneModalVisible, setDoneModalVisible] = useState(false);
+  const showDoneModal = () => setDoneModalVisible(true);
+  const closeDoneModal = () => setDoneModalVisible(false);
+
+  const onPress = () => {
+    closeModal();
+    setTimeout(() => {
+      showDoneModal();
+    }, 500);
+  };
+
   return (
-    <BasicModal modalVisible={modalVisible} closeModal={closeModal}>
-      <Container>
-        <Input
-          title="연락처(카카오톡 아이디 or 전화번호)"
-          input={Contact}
-          placeholder="카카오톡 아이디 혹은 전화번호를 입력해주세요"
-        />
-        <ReasonInput input={Reason} />
-        <Title>신청 후 연락처와 신청사유는 수정이 어려워요.</Title>
-      </Container>
-      <BasicButton text="신청하기" onPress={closeModal} />
-    </BasicModal>
+    <Container>
+      <BasicModal modalVisible={modalVisible} closeModal={closeModal}>
+        <Wrap>
+          <Input
+            title="연락처(카카오톡 아이디 or 전화번호)"
+            input={Contact}
+            placeholder="카카오톡 아이디 혹은 전화번호를 입력해주세요"
+          />
+          <ReasonInput input={Reason} />
+          <Title>신청 후 연락처와 신청사유는 수정이 어려워요.</Title>
+        </Wrap>
+        <BasicButton text="신청하기" onPress={onPress} />
+      </BasicModal>
+      <ApplyDoneModal
+        modalVisible={DoneModalVisible}
+        closeModal={closeDoneModal}
+      />
+    </Container>
   );
 };
 
-const Container = styled.View`
+const Container = styled.View``;
+
+const Wrap = styled.View`
   padding: 24px 0;
   margin-bottom: 50px;
 `;
