a3c3f95d58c13c1d6d6ce96499a39b066780011f
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-05-26 02:40:59 +0900

    [ADD] MyPage_Profile detailContainer.tsx

--- Screen/MyPage_Profile/index.tsx
+++ Screen/MyPage_Profile/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import BasicContainer from '../../Component/MypageContainer';
-import PrintList from './printList';
+import PrintList from './components/printList';
 import ColorButton from '../../Component/BasicButton';
 import { useGoMyPage, useGoMypageProfileFix } from '../../util/navigationHooks';
 import { User } from '../../Data/User';
+import DetailContainer from './components/detailContainer';
+
 const MyPage_Profile = () => {
   const goMyPage = useGoMyPage();
   const goProfileFix = useGoMypageProfileFix();
@@ -16,34 +18,23 @@ const MyPage_Profile = () => {
   const myRegion = User[0].myRegion;
   const myInterest = User[0].myInterest;
 
+  const age = '만' + String(User[0].age) + '세';
   return (
     <BasicContainer headerTitle="내 프로필" display onPress={goMyPage}>
       <MarginContainer>
         <ContentContainer>
-          <SmallSizeContainer>
-            <Title>연령</Title>
-            <Content>만 22세</Content>
-          </SmallSizeContainer>
-          <PrintListContainer>
-            <Title>관심지역</Title>
+          <DetailContainer title="연령" content={age} />
+          <DetailContainer title="관심지역">
             <PrintList lists={myRegion}></PrintList>
-          </PrintListContainer>
-          <PrintListContainer>
-            <Title>관심분야</Title>
+          </DetailContainer>
+          <DetailContainer title="관심분야">
             <PrintList lists={myInterest}></PrintList>
-          </PrintListContainer>
-          <SmallSizeContainer>
-            <Title>나의 상황</Title>
-            <Content>대학생/취준생</Content>
-          </SmallSizeContainer>
-          <BigPrintListContainer>
-            <Title>나의 성격</Title>
+          </DetailContainer>
+          <DetailContainer title="상황" content={User[0].myState.name} />
+          <DetailContainer title="나의 성격">
             <PrintList lists={myCharacter} flexDirection="column"></PrintList>
-          </BigPrintListContainer>
-          <BigSizeContainer>
-            <Title>자기소개</Title>
-            <Content>{User[0].aboutMe}</Content>
-          </BigSizeContainer>
+          </DetailContainer>
+          <DetailContainer title="자기소개" content={User[0].aboutMe} />
         </ContentContainer>
         <ButtonContainer>
           <ColorButton
@@ -65,33 +56,6 @@ const MarginContainer = styled.View`
 
 const ContentContainer = styled.View`
   flex: 12;
-`;
-
-const SmallSizeContainer = styled.View`
-  flex: 1.5;
-`;
-
-const PrintListContainer = styled.View`
-  flex: 2;
-`;
-
-const BigPrintListContainer = styled.View`
-  flex: 4;
-`;
-
-const BigSizeContainer = styled.View`
-  flex: 3;
-`;
-
-const Title = styled.Text`
-  font-size: 12px;
-  color: #b4b4b4;
-  padding-bottom: 5px;
-  padding-top: 10px;
-`;
-
-const Content = styled.Text`
-  font-size: 12px;
 `;
 
 const ButtonContainer = styled.View`
