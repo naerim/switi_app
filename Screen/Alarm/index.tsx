3efec724eadb6cc8d2d9ea714f8c9142c35e17f0
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-06-24 21:47:49 +0900

    [ADD] myPage, Alarm, Notice scroll 설정 변경

--- Screen/Alarm/index.tsx
+++ Screen/Alarm/index.tsx
@@ -4,10 +4,12 @@ import { Switch } from 'react-native';
 import { useGoMyPage } from '../../util/navigationHooks';
 import ColorButton from '../../Component/BasicButton';
 import AlarmContainer from '../../Component/MypageContainer';
+import useScroll from '../../util/useScroll';
 
 const Alarm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const onPress = () => console.log('설정으로 이동');
+  const { scroll, scrollOn } = useScroll();
 
   const [switchValue, setSwitchValue] = useState(false);
   const toggleSwitch = (value: any) => {
@@ -41,86 +43,94 @@ const Alarm = () => {
   const goMyPage = useGoMyPage();
 
   return (
-    <AlarmContainer onPress={goMyPage} display headerTitle="알림설정">
-      <TopTextContainer>
-        <BasicText>이런, 기기 알림이 꺼져있네요!</BasicText>
-      </TopTextContainer>
-      <ButtonContainer>
-        <ColorButton
-          text="기기 알림 설정하기"
-          onPress={onPress}
-          loading={isLoading}
-        />
-      </ButtonContainer>
-      <AllSwithContainer>
-        <SwitchContainer>
-          <SwitchTextContainer>
-            <BasicText>관심 스터디/클래스 알림</BasicText>
-            <SmallText>
-              내 프로필 설정에 맞는 스터디/클래스가 올라왔을 때
-            </SmallText>
-          </SwitchTextContainer>
-          <Switch
-            onValueChange={interestToggleSwitch}
-            value={interestSwitchValue}
-            trackColor={{
-              true: '#86E3C3',
-              false: '#DBDBDB',
-            }}
+    <AlarmContainer
+      onPress={goMyPage}
+      display
+      headerTitle="알림설정"
+      scroll={scroll}
+    >
+      <ScrollContainer onScroll={scrollOn}>
+        <TopTextContainer>
+          <BasicText>이런, 기기 알림이 꺼져있네요!</BasicText>
+        </TopTextContainer>
+        <ButtonContainer>
+          <ColorButton
+            text="기기 알림 설정하기"
+            onPress={onPress}
+            loading={isLoading}
           />
-        </SwitchContainer>
-        <SmallLine />
-        <SwitchContainer>
-          <SwitchTextContainer>
-            <BasicText>스터디/클래스 신청 알림</BasicText>
-            <SmallText>누군가 내 스터디/클래스를 신청했을 때 </SmallText>
-          </SwitchTextContainer>
-          <Switch
-            onValueChange={applyToggleSwitch}
-            value={applySwitchValue}
-            trackColor={{
-              true: '#86E3C3',
-              false: '#DBDBDB',
-            }}
-          />
-        </SwitchContainer>
-        <SmallLine />
-        <SwitchContainer>
-          <SwitchTextContainer>
-            <BasicText>스터디/클래스 수락 알림</BasicText>
-            <SmallText>스터디/클래스 신청이 수락되었을 때</SmallText>
-          </SwitchTextContainer>
-          <Switch
-            onValueChange={acceptToggleSwitch}
-            value={acceptSwitchValue}
-            trackColor={{
-              true: '#86E3C3',
-              false: '#DBDBDB',
-            }}
-          />
-        </SwitchContainer>
-        <SmallLine />
-        <SwitchContainer>
-          <SwitchTextContainer>
-            <BasicText>상호평가 알림</BasicText>
-            <SmallText>
-              스터디 기간 완료 후 스터디원 상호평가가 필요할 때
-            </SmallText>
-          </SwitchTextContainer>
-          <Switch
-            onValueChange={evaluationToggleSwitch}
-            value={evaluationSwitchValue}
-            trackColor={{
-              true: '#86E3C3',
-              false: '#DBDBDB',
-            }}
-          />
-        </SwitchContainer>
-      </AllSwithContainer>
+        </ButtonContainer>
+        <AllSwithContainer>
+          <SwitchContainer>
+            <SwitchTextContainer>
+              <BasicText>관심 스터디/클래스 알림</BasicText>
+              <SmallText>
+                내 프로필 설정에 맞는 스터디/클래스가 올라왔을 때
+              </SmallText>
+            </SwitchTextContainer>
+            <Switch
+              onValueChange={interestToggleSwitch}
+              value={interestSwitchValue}
+              trackColor={{
+                true: '#86E3C3',
+                false: '#DBDBDB',
+              }}
+            />
+          </SwitchContainer>
+          <SmallLine />
+          <SwitchContainer>
+            <SwitchTextContainer>
+              <BasicText>스터디/클래스 신청 알림</BasicText>
+              <SmallText>누군가 내 스터디/클래스를 신청했을 때 </SmallText>
+            </SwitchTextContainer>
+            <Switch
+              onValueChange={applyToggleSwitch}
+              value={applySwitchValue}
+              trackColor={{
+                true: '#86E3C3',
+                false: '#DBDBDB',
+              }}
+            />
+          </SwitchContainer>
+          <SmallLine />
+          <SwitchContainer>
+            <SwitchTextContainer>
+              <BasicText>스터디/클래스 수락 알림</BasicText>
+              <SmallText>스터디/클래스 신청이 수락되었을 때</SmallText>
+            </SwitchTextContainer>
+            <Switch
+              onValueChange={acceptToggleSwitch}
+              value={acceptSwitchValue}
+              trackColor={{
+                true: '#86E3C3',
+                false: '#DBDBDB',
+              }}
+            />
+          </SwitchContainer>
+          <SmallLine />
+          <SwitchContainer>
+            <SwitchTextContainer>
+              <BasicText>상호평가 알림</BasicText>
+              <SmallText>
+                스터디 기간 완료 후 스터디원 상호평가가 필요할 때
+              </SmallText>
+            </SwitchTextContainer>
+            <Switch
+              onValueChange={evaluationToggleSwitch}
+              value={evaluationSwitchValue}
+              trackColor={{
+                true: '#86E3C3',
+                false: '#DBDBDB',
+              }}
+            />
+          </SwitchContainer>
+        </AllSwithContainer>
+      </ScrollContainer>
     </AlarmContainer>
   );
 };
 
+const ScrollContainer = styled.ScrollView``;
 const TopTextContainer = styled.View`
   flex: 1;
   align-items: center;
