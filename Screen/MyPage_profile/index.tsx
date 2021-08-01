6602e2e910a12c7469fd99a601d677b0661b83e9
naerim <naerim1119@gmail.com>
2021-08-01 13:23:06 +0900

    [FIX] myPage profile

--- Screen/MyPage_profile/index.tsx
+++ Screen/MyPage_profile/index.tsx
@@ -1,129 +1,71 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import BasicContainer from '../../Component/BasicContainer';
+import BasicContainer from '../../Component/ContainerWithBack';
 import PrintList from './components/printList';
 import ColorButton from '../../Component/BasicButton';
-import { useGoMyPage } from '../../util/navigationHooks';
+import { useGoMyPage, useGoMypageProfileFix } from '../../util/navigationHooks';
+import { User } from '../../Data/User';
+import DetailContainer from './components/detailContainer';
+import useScroll from '../../util/useScroll';
 
 const MyPage_profile = () => {
+  const { scroll, scrollOn } = useScroll();
   const goMyPage = useGoMyPage();
+  const goProfileFix = useGoMypageProfileFix();
 
   const [isLoading, setIsLoading] = useState(false);
-  const [onPress, setonPress] = useState(false);
 
-  const myCharacter = [
-    {
-      key: 1,
-      name: '체계적이고 계획적인 사람이에요',
-    },
-    {
-      key: 2,
-      name: '솔직하고 시원시원해요',
-    },
-    {
-      key: 3,
-      name: '시간을 잘 지켜요',
-    },
-  ];
+  const myCharacter = User[0].myCharacter;
 
-  const interestedState = [
-    {
-      key: 1,
-      name: '서울 전체',
-    },
-  ];
+  const myRegion = User[0].myRegion;
+  const myInterest = User[0].myInterest;
 
-  const interestedParts = [
-    {
-      key: 1,
-      name: '취업',
-    },
-    {
-      key: 2,
-      name: '어학',
-    },
-  ];
+  const age = '만' + String(User[0].age) + '세';
   return (
-    <BasicContainer headerTitle="내 프로필" display onPress={goMyPage}>
-      <MarginContainer2>
-        <ContentContainer>
-          <SmallSizeContainer>
-            <Title>연령</Title>
-            <Content>만 22세</Content>
-          </SmallSizeContainer>
-          <PrintListContainer>
-            <Title>관심지역</Title>
-            <PrintList lists={interestedState} />
-          </PrintListContainer>
-          <PrintListContainer>
-            <Title>관심분야</Title>
-            <PrintList lists={interestedParts} />
-          </PrintListContainer>
-          <SmallSizeContainer>
-            <Title>나의 상황</Title>
-            <Content>대학생/취준생</Content>
-          </SmallSizeContainer>
-          <BigPrintListContainer>
-            <Title>나의 성격</Title>
-            <PrintList lists={myCharacter} flexDirection="column" />
-          </BigPrintListContainer>
-          <BigSizeContainer>
-            <Title>자기소개</Title>
-            <Content>
-              취미, 희망직무 등 나에 대해 간단히 소개해 주세요. 취미, 희망직무
-              등 나에 대해 간단히 소개해 주세요. 취미, 희망직무 등 나에 대해
-              간단히 소개해 주세요. 취미, 희망직무 등 나에 대해 간단히 소개해
-              주세요. 취미, 희망직무 등 나에 대해 간단히 소개해 주세요.
-            </Content>
-          </BigSizeContainer>
-        </ContentContainer>
+    <BasicContainer
+      headerTitle="내 프로필"
+      display
+      onPress={goMyPage}
+      scroll={scroll}
+    >
+      <MarginContainer>
+        <ScrollContainer onScroll={scrollOn}>
+          <DetailContainer title="연령" content={age} />
+          <DetailContainer title="관심지역">
+            <PrintList lists={myRegion}></PrintList>
+          </DetailContainer>
+          <DetailContainer title="관심분야">
+            <PrintList lists={myInterest}></PrintList>
+          </DetailContainer>
+          <DetailContainer title="상황" content={User[0].myState.name} />
+          <DetailContainer title="나의 성격">
+            <PrintList lists={myCharacter} flexDirection="column"></PrintList>
+          </DetailContainer>
+          <DetailContainer title="자기소개" content={User[0].aboutMe} />
+        </ScrollContainer>
         <ButtonContainer>
-          <ColorButton text="수정하기" onPress={onPress} loading={isLoading} />
+          <ColorButton
+            text="수정하기"
+            onPress={goProfileFix}
+            loading={isLoading}
+          />
         </ButtonContainer>
-      </MarginContainer2>
+      </MarginContainer>
     </BasicContainer>
   );
 };
 
-const MarginContainer2 = styled.View`
+const MarginContainer = styled.View`
   margin-left: 24px;
   margin-right: 24px;
   flex: 1;
+  justify-content: space-between;
 `;
 
-const ContentContainer = styled.View`
-  flex: 12;
+const ScrollContainer = styled.ScrollView`
+  margin-top: 10px;
 `;
 
-const SmallSizeContainer = styled.View`
-  flex: 1.5;
-`;
-
-const PrintListContainer = styled.View`
-  flex: 2;
-`;
-
-const BigPrintListContainer = styled.View`
-  flex: 4;
-`;
-
-const BigSizeContainer = styled.View`
-  flex: 3;
-`;
-
-const Title = styled.Text`
-  font-size: 12px;
-  color: #b4b4b4;
-  padding-bottom: 5px;
-  padding-top: 10px;
-`;
-
-const Content = styled.Text`
-  font-size: 12px;
-`;
-
-const ButtonContainer = styled.View`
-  flex: 1;
-`;
+const ButtonContainer = styled.View``;
 
 export default MyPage_profile;
