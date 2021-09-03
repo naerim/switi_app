7ad4d28d0f3602b02e899b9b1652483580bd7d08
naerim <naerim1119@gmail.com>
2021-09-03 09:43:47 +0900

    [ADD] flatlist 이미지 설정

--- Screen/Home/components/StudyFlatList/StudyImage.tsx
+++ Screen/Home/components/StudyFlatList/StudyImage.tsx
@@ -8,9 +8,14 @@ interface Props {
 }
 
 const StudyImage: React.FC<Props> = ({ img, done }) => {
+  // 이미지 불러오기
+  const loadImg = (url: string) => {
+    return 'http://localhost:4000/images/' + url;
+  };
+
   return (
     <Container>
-      <Images source={{ uri: img }} style={{ width: 10, height: 10 }} />
+      <Images source={{ uri: loadImg(img) }} resizeMode="stretch" />
       <RecruitIcon done={!done} />
     </Container>
   );
@@ -18,15 +23,19 @@ const StudyImage: React.FC<Props> = ({ img, done }) => {
 
 const Container = styled.View`
   flex: 1;
-  width: 84px;
-  height: 84px;
   align-items: center;
   justify-content: center;
-  border-radius: 9px;
+  width: 84px;
+  height: 84px;
+  margin-top: 12px;
 `;
 
 const Images = styled.Image`
-  background-color: aquamarine;
+  background-color: #ffeec9;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  border-radius: 9px;
 `;
 
 export default StudyImage;
