e1c6159f793830a26a7ac5d0ee09803528053e65
naerim <naerim1119@gmail.com>
2021-11-07 21:11:27 +0900

    [FIX] 공지사항 key 오류 수정

--- Screen/MyPage_Notice/index.tsx
+++ Screen/MyPage_Notice/index.tsx
@@ -3,7 +3,6 @@ import NoticeContainer from '../../Component/ContainerWithBack';
 import { useGoMyPage, UseGoNoticeDetail } from '../../util/navigationHooks';
 import styled from 'styled-components/native';
 import { NoticeData } from './NoticeData';
-import OneNoticeContainer from './OneNoticeContainer';
 import useScroll from '../../util/useScroll';
 
 const MyPage_Notice = () => {
@@ -19,12 +18,11 @@ const MyPage_Notice = () => {
     >
       <AllNoticeContainer onScroll={scrollOn}>
         {NoticeData.map(({ key, title, createAt, Component }) => (
-          <OneNoticeContainer
-            key={key}
-            onPress={goNotice(key)}
-            title={title}
-            createAt={createAt}
-          />
+          <Container activeOpacity={0.8} onPress={goNotice(key)} key={key}>
+            <Title>{title}</Title>
+            <Date>{createAt}</Date>
+            <Line />
+          </Container>
         ))}
       </AllNoticeContainer>
     </NoticeContainer>
@@ -33,6 +31,29 @@ const MyPage_Notice = () => {
 
 const AllNoticeContainer = styled.ScrollView`
   flex: 1;
+`;
+
+const Container = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+const Title = styled.Text`
+  font-size: 12px;
+  padding-bottom: 5px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+const Date = styled.Text`
+  font-size: 10px;
+  color: #b4b4b4;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-bottom: 20px;
+`;
+
+const Line = styled.View`
+  height: 1px;
+  background-color: #f3f3f3;
 `;
 
 export default MyPage_Notice;
