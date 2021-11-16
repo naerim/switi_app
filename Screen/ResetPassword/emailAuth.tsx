3179bb5e11d70334b08a00e81f647aa0df5516bb
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-16 11:17:08 +0900

    [FIX] 비밀번호 찾기 이메일 보내기 api 연결

--- Screen/ResetPassword/emailAuth.tsx
+++ Screen/ResetPassword/emailAuth.tsx
@@ -8,8 +8,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { rootState } from '../../redux';
 import { findPwdThunk } from '../../redux/authReducer';
 import { emailCheck } from '../../Component/authFunction';
+import axios from 'axios';
+import { Alert } from 'react-native';
 
 const EmailAuth = () => {
+  const email = useInput('');
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+  const urlencoded = new URLSearchParams();
+  urlencoded.append('email', 'mn0316@naver.com');
+
   const dispatch = useDispatch();
   const { findPwdSuccess } = useSelector((state: rootState) => ({
     findPwdSuccess: state.authReducer.findPwdSuccess,
@@ -18,15 +28,41 @@ const EmailAuth = () => {
   const goCertification = useGoCertification();
   const desc =
     '입력하신 이메일 주소로 인증번호가 전송됩니다.\n인증이 완료된 후 비밀번호를 재설정해주세요.';
-  const email = useInput('');
 
   const getNumber = async () => {
-    if (emailCheck(email.value)) {
-      await dispatch(findPwdThunk(email.value));
-      if (findPwdSuccess) {
-        await goCertification;
-      }
-    }
+    // if (emailCheck(email.value)) {
+    //   await dispatch(findPwdThunk(email.value));
+    //   if (findPwdSuccess) {
+    //     await goCertification;
+    //   }
+    // }
+    // const requestOptions = {
+    //   method: 'POST',
+    //   headers: myHeaders,
+    //   body: urlencoded,
+    //   redirect: 'follow',
+    // };
+    // // @ts-ignore
+    // fetch('http://localhost:4000/auth/findPwd', requestOptions)
+    //   .then((response) => response.text())
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log('error', error));
+    axios({
+      method: 'post',
+      url: 'http://localhost:4000/auth/findPwd',
+      data: { email: email.value },
+    })
+      .then((res) => {
+        // setModalVisible(false);
+        setTimeout(() => {
+          // setDoneModalVisible(true);
+        }, 500);
+      })
+      .catch((err) => {
+        if (err.toString() == 'Error: Request failed with status code 404')
+          Alert.alert('인증번호가 일치하지 않습니다.');
+        else Alert.alert('이메일 인증 오류 :(');
+      });
   };
 
   return (
