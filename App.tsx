949bdf4c121c646c491e2567755370f6bbfc12e6
naerim <naerim1119@gmail.com>
2021-11-22 23:51:17 +0900

    [FIX] 폰트 삭제

--- App.tsx
+++ App.tsx
@@ -2,22 +2,9 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { useFonts } from 'expo-font';
 import RootNavigator from './Screen/Navigator';
 
 const App = () => {
-  const [loaded] = useFonts({
-    'NotoSans': require('./assets/fonts/NotoSans-Regular.ttf'),
-    'NotoSans-Light': require('./assets/fonts/NotoSans-Light.ttf'),
-    'NotoSans-Bold': require('./assets/fonts/NotoSans-Bold.ttf'),
-    'NotoSans-SemiBold': require('./assets/fonts/NotoSans-SemiBold.ttf'),
-    'NotoSans-Medium': require('./assets/fonts/NotoSans-Medium.ttf'),
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
--- Component/Font/index.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const ContentFonts = styled.Text`
-  color: #2b2b2b;
-  font-size: 12px;
-`;
